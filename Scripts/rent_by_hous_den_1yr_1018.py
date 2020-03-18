c214f9a27ce60157c2b7aa2542bb6f87ac4b40bf
Hassen Morad <hassen.morad@gmail.com>
2020-03-18 13:27:15 -0700

    Add files via upload

--- Scripts/rent_by_hous_den_1yr_1018.py
+++ Scripts/rent_by_hous_den_1yr_1018.py
@@ -12,7 +12,8 @@ for year in range(2010, 2019):
     yr_df.Total_Rented = yr_df.Total_Rented.astype(int)
     
     # FIPS grouped by county population density
-    homes75 = yr_df.FIPS[(yr_df.Year == year) & (yr_df.Housing_Den > 75)].values
+    homes200 = yr_df.FIPS[(yr_df.Year == year) & (yr_df.Housing_Den > 200)].values
+    homes75 = yr_df.FIPS[(yr_df.Year == year) & (yr_df.Housing_Den > 75) & (yr_df.Housing_Den <= 200)].values
     homes30 = yr_df.FIPS[(yr_df.Year == year) & (yr_df.Housing_Den > 30) & (yr_df.Housing_Den <= 75)].values
     homes20 = yr_df.FIPS[(yr_df.Year == year) & (yr_df.Housing_Den > 20) & (yr_df.Housing_Den <= 30)].values
     homes10 = yr_df.FIPS[(yr_df.Year == year) & (yr_df.Housing_Den > 10) & (yr_df.Housing_Den <= 20)].values
@@ -20,8 +21,8 @@ for year in range(2010, 2019):
     homesless5 = yr_df.FIPS[yr_df.Housing_Den <= 5].values
     
     groups_dict = {}
-    fips_groups = [homes75, homes30, homes20, homes10, homes5, homesless5]
-    groups = ['75', '30', '20', '10', '5', 'Less5']
+    fips_groups = [homes200, homes75, homes30, homes20, homes10, homes5, homesless5]
+    groups = ['200', '75', '30', '20', '10', '5', 'Less5']
     counter = 0
     for fips_group in fips_groups:
         fips_rent = array.array('f')
@@ -35,7 +36,7 @@ for year in range(2010, 2019):
 
 df = pd.DataFrame(yrs_dict)
 df = df.transpose()
-df.columns = ['More than 75', '30 to 75', '20 to 30', '10 to 20', '5 to 10', 'Less than 5']
+df.columns = ['More than 200', '75 to 200', '30 to 75', '20 to 30', '10 to 20', '5 to 10', 'Less than 5']
 df['Year'] = list(range(2010, 2019))
 df = df.melt(id_vars='Year', var_name='Hous_Den', value_name='Med_Rent')
 df.to_csv('rent_by_hous_den_1yr_1018.csv', index=False)
